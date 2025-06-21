b10a7e71e95f7e071990a1d36359b6874e08ba5f
Kevin Cooke <kevinc222@outlook.com>
2025-06-21 12:37:39 -0700

    Complete 8/11 linked list problems

--- add_two_numbers_dummy.py
+++ add_two_numbers_dummy.py
@@ -6,26 +6,23 @@
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # iterate through, track the carry
-        head = ListNode()
-        curr = head
-        carry = 0
+        dummy = ListNode()
+        cur = dummy
 
+        carry = 0
         while l1 or l2 or carry:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
 
-            curr.val = (v1 + v2 + carry) % 10
-            carry = (v1 + v2 + carry) // 10
+            # new digit
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
 
+            # update ptrs
+            cur = cur.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
 
-            # next node
-            if l1 or l2 or carry:
-                new_node = ListNode()
-                curr.next = new_node
-                curr = new_node
-
-
-        return head
+        return dummy.next
