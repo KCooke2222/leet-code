3339d82cd07004d0f6cc24e4cfffda753b5bb8bd
Kevin Cooke <kevinc222@outlook.com>
2025-08-15 08:51:40 -0700

    Complete bit problems

---
+++ Complete/reverse_bits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+
+        for i in range(32):
+            res <<= 1
+            
+            if n % 2 == 1:
+                res += 1
+
+            n >>= 1
+            
+
+        return res
